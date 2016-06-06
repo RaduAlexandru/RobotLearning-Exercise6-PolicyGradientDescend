794ff1cfa72fcb6a098c832cf86dcea2342e89bd
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2016-06-06 10:42:44 +0200

    save arezzo files

--- cart.py
+++ cart.py
@@ -4,20 +4,20 @@ import time
 canv_width=640
 canv_height=480
 
-cart_width=150
+cart_width=100
 cart_height=100
 
-pole_height=50
+pole_height=150
 
 
 class cartObj(object):
 
-    def __init__(self,x,y,width,height):
+    def __init__(self,p1_x,p1_y,p2_x,p2_y):
         print "Creating cart"
-        self.x=x
-        self.y=y
-        self.width=width
-        self.height=height
+        self.p1_x=p1_x
+        self.p1_y=p1_y
+        self.p2_x=p2_x
+        self.p2_y=p2_y
 
     def apple(self):
         #print self.tangerine
@@ -40,19 +40,18 @@ def main():
     canv.pack(fill='both', expand=True)
 
     #Create objects
-    cart = cartObj(canv_width/2, 0, cart_width, cart_height) #top_left corner x and y, right bottom point x and y
-    pole = poleObj(canv_width/2-cart_width/2, cart_height, canv_width/2-cart_width/2, cart_height+pole_height) #point 1 x and y, point 2 x and y
-
+    cart = cartObj(canv_width/2-cart_width/2, canv_height-cart_height, canv_width/2+cart_width/2, canv_height) #top_left corner x and y, right bottom point x and y
+    pole = poleObj(canv_width/2, canv_height-cart_height, canv_width/2, canv_height-cart_height-pole_height) #point 1 x and y, point 2 x and y
+    #pole = poleObj(canv_width/2, 0, canv_width/2, 640)
 
     #Draw objects
     middle = canv.create_line(canv_width/2, 0, canv_width/2, 640, fill='red')
     right = canv.create_line(canv_width, 0, canv_width, canv_height, fill='blue')
     left = canv.create_line(0, 0, 0, canv_height, fill='red')
 
-
-    cart = canv.create_rectangle(canv_width , canv_height , 100, 100, outline='black', fill='gray40', tags=('rect'))
-    #cart = canv.create_rectangle(cart.x, cart.y , cart.width, cart.height, outline='black', fill='gray40', tags=('rect'))
+    #cart = canv.create_rectangle(cart.p1_x, cart.p1_y , cart.p2_x, cart.p2_y, outline='black', fill='gray40', tags=('rect'))
     pole = canv.create_line(pole.p1_x, pole.p1_y, pole.p2_x, pole.p1_y, fill='red')
+    #pole = canv.create_line(canv_width/2, 0, canv_width/2, 640, fill='red')
 
 
     #cart.apple()
